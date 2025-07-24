da461d266f5f20260cc23077cc08ae30cfcb0901
Renato <renatolhernandez@gmail.com>
2025-07-24 21:03:14 +0000

    ajustes nos models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,29 @@
 class Comment < ApplicationRecord
+  include AASM
+
   belongs_to :post
+
+  validates :body, :email, :name, presence: true
+
+  aasm column: 'status' do
+    state :new, initial: true
+    state :processing
+    state :approved
+    state :rejected
+
+    event :start_processing do
+      transitions from: :new, to: :processing
+    end
+
+    event :approve do
+      transitions from: :processing, to: :approved
+    end
+
+    event :reject do
+      transitions from: :processing, to: :rejected
+    end
+  end
+
+  scope :approved, -> { where(status: 'approved') }
+  scope :rejected, -> { where(status: 'rejected') }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :comments, through: :posts
+  has_one :analysis, dependent: :destroy
+
+  validates :username, presence: true
 end
